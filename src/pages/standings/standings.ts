abe21f0f7db42e786fae6e46292df47775c88603
Joanne Edmiston <joanneedmiston@grofar.com>
2019-01-06 15:24:12 +0000

    Tutorial  Module completed - navigation

---
+++ src/pages/standings/standings.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the StandingsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-standings',
+  templateUrl: 'standings.html',
+})
+export class StandingsPage {
+
+  public team: any = {};
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.team = this.navParams.data;
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad StandingsPage');
+  }
+
+}
